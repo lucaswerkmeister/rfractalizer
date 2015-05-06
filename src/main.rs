cb2d04d8f873bdf9175135df032c1b700fe22d6e
Lucas Werkmeister <mail@lucaswerkmeister.de>
2015-05-06 13:08:29 +0200

    Initial GTK code (closable window)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate gtk;
+
+use gtk::traits::*;
+use gtk::widgets;
+use gtk::signal::Inhibit;
+
 fn main() {
+    gtk::init();
     
+    let window = widgets::Window::new(gtk::WindowType::TopLevel).unwrap();
+    window.set_title("RFractalizer");
+    window.set_window_position(gtk::WindowPosition::Center);
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(true)
+    });
+    
+    window.show_all();
+    gtk::main();
 }
